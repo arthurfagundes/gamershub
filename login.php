d8e69a70298f760ce15740945b86706763c26d9f
João Vitor Pacheco <jvitor.pacheco@rede.ulbra.br>
2023-11-17 21:27:05 -0300

    login17/11

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Entrar</h1>
+
+    <form action="" method="post">
+<input type="email" name="email" placeholder="Digite o email">
+
+<input type="password" name="senha" placeholder="Senha:">
+
+
+<input type="submit" value="Entrar">
+</form>
+
+
+<?php
+session_start();
+include 'confg.php';
+include 'User.php';
+
+
+$user = new User($conn);
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+
+    if ($user->login($email, $senha)) {
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        echo "Login falhou. Verifique suas credenciais.";
+    }
+}
+?>
+
+</body>
+</html>
