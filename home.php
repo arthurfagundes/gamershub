63d6cbb0148b5d2e3b1fbf97bdd48a14a5b93478
Luis Henrique Severo da Silva <luisseverodasilva17@gmail.com>
2023-11-21 22:16:19 -0300

    Update home.php

--- home.php
+++ home.php
@@ -28,6 +28,9 @@ $idUsuarioLogado = $_SESSION['id'];
     <link rel="stylesheet" type="text/css" href="./css/home.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap"         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Document</title>
