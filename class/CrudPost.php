125c6cf38b61aa4cb5654cde8cff4a9a924c8dd8
Luis Henrique Severo da Silva <luisseverodasilva17@gmail.com>
2023-11-21 22:06:29 -0300

    0.1.02 MENU
    Adições e alterações na tela menu para melhorais de interface e ideias de projeto

---
+++ class/CrudPost.php
@@ -0,0 +1,114 @@
+<?php
+
+class CrudPost
+{
+    private $conn;
+    private $table_name = "posts";
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function adicionarPost($titulo, $texto, $usuario_id, $imagem)
+    {
+        try {
+            // Removed game ID from query
+            $query = "INSERT INTO " . $this->table_name . " (titulo, texto, usuario_id, imagem) VALUES (?, ?, ?, ?)";
+            $stmt = $this->conn->prepare($query);
+            // Setting likes to 0 by default
+            $stmt->execute([$titulo, $texto, $usuario_id, $imagem, 0]);
+
+            echo "Postagem criada com sucesso!";
+        } catch (PDOException $e) {
+            echo "Erro ao criar postagem: " . $e->getMessage();
+        }
+    }
+
+    public function curtirPost($id)
+    {
+        try {
+            // Removed game ID from query
+            $query = "UPDATE " . $this->table_name . " SET curtidas = curtidas + 1 WHERE id = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$id]);
+
+            echo "Postagem curtida com sucesso!";
+        } catch (PDOException $e) {
+            echo "Erro ao curtir postagem: " . $e->getMessage();
+        }
+    }
+
+    public function deletarPost($id)
+    {
+        try {
+            $query = "DELETE FROM " . $this->table_name . " WHERE id=?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$id]);
+
+            echo "Postagem excluída com sucesso!";
+        } catch (PDOException $e) {
+            echo "Erro ao excluir postagem: " . $e->getMessage();
+        }
+    }
+
+    public function exibirPost($id)
+    {
+        try {
+            $query = "SELECT * FROM " . $this->table_name . " WHERE id = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$id]);
+
+            $post = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($post) {
+                echo "<h1>" . $post['titulo'] . "</h1>";
+                echo "<p>" . $post['texto'] . "</p>";
+                echo "<p>Curtidas: " . $post['curtidas'] . "</p>";
+            } else {
+                echo "Postagem não encontrada";
+            }
+        } catch (PDOException $e) {
+            echo "Erro ao exibir postagem: " . $e->getMessage();
+        }
+    }
+
+    public function buscarPostPorId($id)
+    {
+        try {
+            $query = "SELECT * FROM " . $this->table_name . " WHERE id = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute([$id]);
+
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erro ao buscar postagem por ID: " . $e->getMessage();
+        }
+    }
+
+    public function listarPostagens()
+    {
+        try {
+            $query = "SELECT * FROM " . $this->table_name;
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+
+            $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($posts) {
+                foreach ($posts as $post) {
+                    echo "<h1>" . $post['titulo'] . "</h1>";
+                    echo "<p>" . $post['texto'] . "</p>";
+                    echo "<p>Curtidas: " . $post['curtidas'] . "</p>";
+                    echo "<hr>";
+                }
+            } else {
+                echo "Sem postagens encontradas";
+            }
+
+            return $posts;
+        } catch (PDOException $e) {
+            echo "Erro ao exibir postagens: " . $e->getMessage();
+        }
+    }
+}
