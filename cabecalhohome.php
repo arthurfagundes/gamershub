112a11e5e30b87e176599f2199c9b06d495835c1
Luis Henrique Severo da Silva <luisseverodasilva17@gmail.com>
2023-11-24 20:09:12 -0300

    Ups HOME

---
+++ cabecalhohome.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <title>GamersHub</title>
+</head>
+
+<body>
+    <nav>
+        <img class="logo" src="./img/logogamershub.png" height="80">
+        <ul>
+            <li><a href="./perfil.php">Perfil</a></li>
+            <li><a href="./jogos.php">Jogos</a></li>
+            <li><a href="./login.php" onclick="$CrudUsuario.sair()">Sair</a></li>
+        </ul>
+    </nav>
